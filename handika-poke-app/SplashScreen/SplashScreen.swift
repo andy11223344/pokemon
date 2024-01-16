dcebbf33ed45831905763675f0318c5cef69408b
andy <handikasyahputra93@gmail.com>
2024-01-16 16:45:57 +0700

    Complete splash screen

---
+++ handika-poke-app/SplashScreen/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  handika-poke-app
+//
+//  Created by Andy ⠀ on 16/01/24.
+//
+
+import UIKit
+
+class SplashScreen: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
